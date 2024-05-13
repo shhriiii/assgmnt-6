28b129f1e079e36a12bd4e1b530d7bc59ae90878
shhriiii <149945420+shhriiii@users.noreply.github.com>
2024-05-13 23:26:35 +0530

    Add files via upload

---
+++ assgmnt6/q2.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+double area(int radius) {
+return 3.14 * radius * radius;
+}
+int main() {
+int radius;
+cin >> radius;
+cout << "Area of the circle is: " << area(radius) << " units square";
+return 0;
+}
